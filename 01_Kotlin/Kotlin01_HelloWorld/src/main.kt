ba3371caf179ed84fc6815b4c9d82e537d2b3144
rmadmsjm <dmswjdals12@naver.com>
2023-12-27 15:20:54 +0900

    231227 기본 문법

---
+++ 01_Kotlin/Kotlin01_HelloWorld/src/main.kt
@@ -0,0 +1,14 @@
+/*
+실행...
+개발자는 Kotlin 문법에 맞춰 코드를 작성함
+Kotlin 코드를 실행하면 Kotlin 코드는 Java 코드로 변경됨
+Java 코드를 컴파일하여 class 코드로 변경함
+class 코드를 JVM(Java Virtual Machine)으로 전달하여 OS 에 맞는 기계어로 번역하며 실행함
+이 때, fun main() 이라고 되어 있는 부분을 찾아 그 부분의 코드를 동작시킴
+개발자 입장에서 프로그램의 시작 부분은 fun main() 부분이 됨
+ */
+
+fun main() {
+    println("Hello World")
+    println("안녕하세요")
+}
