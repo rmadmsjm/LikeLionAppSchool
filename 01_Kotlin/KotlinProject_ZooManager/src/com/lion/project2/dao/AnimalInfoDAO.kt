d2fd54c63682771ba0c109bfd76ef2c2958fe303
rmadmsjm <dmswjdals12@naver.com>
2024-01-15 10:35:54 +0900

    feat: AnimalInfoDAO, AnimalModel

--- 01_Kotlin/KotlinProject_ZooManager/src/com/lion/project2/dao/AnimalInfoDAO.kt
+++ 01_Kotlin/KotlinProject_ZooManager/src/com/lion/project2/dao/AnimalInfoDAO.kt
@@ -1,4 +1,53 @@
 package com.lion.project2.dao
 
+import com.lion.project2.model.AnimalModel
+import java.io.*
+
 class AnimalInfoDAO {
+
+    companion object {
+        // 파일이 있는지 확인하는 메서드
+        fun isExistUserInfoFile() : Boolean {
+            // File 객체 생성
+            val saveFile = File("animalData.dat")
+            // 파일이 존재하는지 확인
+            // 파일이 있으면 true를 반환하고 없으면 false를 반환함
+            return saveFile.exists()
+        }
+
+        // 파일에서 데이터를 읽어와 ArrayList에 담고 반환함
+        fun getStudentInfoList(): ArrayList<AnimalModel>? {
+            // 파일이 없다면 null 반환함
+            if (isExistUserInfoFile() == false) {
+                return null
+            }
+            // 파일에서 데이터를 읽어올 기본 스트림을 생성.
+            val fileInputStream = FileInputStream("animalData.dat")
+            // 읽어온 데이터를 객체로 복원하는 필터 스트림을 생성
+            val objectInputStraem = ObjectInputStream(fileInputStream)
+            // 읽어오기
+            val resultList = objectInputStraem.readObject() as ArrayList<AnimalModel>
+            // 파일을 닫기
+            objectInputStraem.close()
+            fileInputStream.close()
+
+            // 객체 반환
+            return resultList
+        }
+
+        // 데이터를 파일에 씀
+        fun saveStudentInfoList(dataList: ArrayList<AnimalModel>) {
+            // 파일에 데이터를 쓰기 위한 기본 스트림 생성
+            // 쓰기를 할 때는 파일이 없을 경우 파일이 생성됨
+            var fileOutputStream = FileOutputStream("animalData.dat")
+            // 객체를 파일에 쓸 수 있는 형태로 가공하는 필터스트림을 생성해서 연결함
+            val objectOutputStream = ObjectOutputStream(fileOutputStream)
+            // 객체를 파일에 씀
+            objectOutputStream.writeObject(dataList)
+            // 파일 닫기
+            objectOutputStream.flush()
+            objectOutputStream.close()
+            fileOutputStream.close()
+        }
+    }
 }
--- 01_Kotlin/KotlinProject_ZooManager/src/com/lion/project2/model/AnimalModel.kt
+++ 01_Kotlin/KotlinProject_ZooManager/src/com/lion/project2/model/AnimalModel.kt
@@ -1,4 +1,25 @@
 package com.lion.project2.model
 
-class AnimalModel {
+class AnimalModel(var type:Int, var name:String, var age:Int, var furCnt:Int, var stripesCnt: Int, var noseLength:Int) {
+    fun printAnimalInfo() {
+        if(type == 1) {
+            println()
+            println("동물 타입 : 사자")
+            println("동물 이름 : $name ")
+            println("동물 나이 : $age ")
+            println("털의 개수 : $furCnt")
+        } else if(type == 2) {
+            println()
+            println("동물 타입 : 호랑이")
+            println("동물 이름 : $name ")
+            println("동물 나이 : $age ")
+            println("줄무늬 개수 : $stripesCnt")
+        } else{
+            println()
+            println("동물 타입 : 코끼리")
+            println("동물 이름 : $name ")
+            println("동물 나이 : $age ")
+            println("코의 길이 : $noseLength")
+        }
+    }
 }
