379647407008ce4c2b5c921950ee19a26d4572db
rmadmsjm <dmswjdals12@naver.com>
2024-01-15 09:12:13 +0900

    feat: 프로그램 상태 정의

--- 01_Kotlin/KotlinProject_ZooManager/src/com/lion/project2/util/ValueUtil.kt
+++ 01_Kotlin/KotlinProject_ZooManager/src/com/lion/project2/util/ValueUtil.kt
@@ -1,4 +1,16 @@
 package com.lion.project2.util
 
-class ValueUtil {
+enum class ProgramState {
+    // 메뉴를 보여주는 상태
+    PROGRAM_STATE_SHOW_MENU,
+    //학생 정보를 입력하는 상태
+    PROGRAM_STATE_INPUT_STUDENT_INFO,
+    // 학생 정보를 검색하는 상태
+    PROGRAM_STATE_SEARCH_STUDENT_INFO,
+    // 학생 정보 전체를 출력하는 상태
+    PROGRAM_STATE_SHOW_STUDENT_INFO_ALL,
+    // 각 과목별 총점과 평균을 확인하는 상태
+    PROGRAM_STATE_SHOW_POINT_TOTAL_AVG,
+    // 종료 상태
+    PROGRAM_STATE_TERMINATE
 }
