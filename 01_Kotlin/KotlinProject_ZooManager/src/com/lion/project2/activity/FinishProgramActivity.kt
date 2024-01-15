6d82a094a0dac1f1485f59a330739faec2e2836e
rmadmsjm <dmswjdals12@naver.com>
2024-01-15 10:49:08 +0900

    feat: activity 클래스 오버라이드

--- 01_Kotlin/KotlinProject_ZooManager/src/com/lion/project2/activity/FinishProgramActivity.kt
+++ 01_Kotlin/KotlinProject_ZooManager/src/com/lion/project2/activity/FinishProgramActivity.kt
@@ -1,4 +1,15 @@
 package com.lion.project2.activity
 
 class FinishProgramActivity : BaseActivity() {
+    override fun initActivity() {
+    }
+
+    override fun processActivity() {
+    }
+
+    override fun showActivity() {
+    }
+
+    override fun finishActivity() {
+    }
 }
--- 01_Kotlin/KotlinProject_ZooManager/src/com/lion/project2/activity/ShowAllAnimalInfoActivity.kt
+++ 01_Kotlin/KotlinProject_ZooManager/src/com/lion/project2/activity/ShowAllAnimalInfoActivity.kt
@@ -1,4 +1,16 @@
 package com.lion.project2.activity
 
 class ShowAllAnimalInfoActivity : BaseActivity() {
+
+    override fun initActivity() {
+    }
+
+    override fun processActivity() {
+    }
+
+    override fun showActivity() {
+    }
+
+    override fun finishActivity() {
+    }
 }
