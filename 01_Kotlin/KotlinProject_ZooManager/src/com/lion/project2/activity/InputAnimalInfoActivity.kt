9762864370c92c4f1b2bd7b2f1f62cc1f2aa722f
rmadmsjm <dmswjdals12@naver.com>
2024-01-15 10:45:33 +0900

    feat: InputAnimalInfoActivity

--- 01_Kotlin/KotlinProject_ZooManager/src/com/lion/project2/activity/InputAnimalInfoActivity.kt
+++ 01_Kotlin/KotlinProject_ZooManager/src/com/lion/project2/activity/InputAnimalInfoActivity.kt
@@ -1,15 +1,24 @@
 package com.lion.project2.activity
 
 import com.lion.project2.controller.MainController
+import com.lion.project2.dao.AnimalInfoDAO
+import com.lion.project2.model.AnimalModel
 import com.lion.project2.util.ProgramState
 import java.util.Scanner
 
 class InputAnimalInfoActivity(var mainController: MainController) : BaseActivity() {
 
     lateinit var scanner: Scanner
+    // 입력된 동물 정보 담을 객체
+    var animalList:ArrayList<AnimalModel>? = null
+    // 동물 정보를 입력 받을 객체
+    lateinit var animalModel: AnimalModel
 
     override fun initActivity() {
         scanner = Scanner(System.`in`)
+
+        // 동물 정보 가져오기
+        animalList = AnimalInfoDAO.getAnimalInfoList()
     }
 
     override fun processActivity() {
@@ -18,6 +27,9 @@ class InputAnimalInfoActivity(var mainController: MainController) : BaseActivity
     override fun showActivity() {
         println()
         println("[ 동물 정보 입력 ]")
+        showAnimalCnt()
+        inputAnimalInfo()
+        saveAnimalInfo()
     }
 
     override fun finishActivity() {
@@ -25,7 +37,11 @@ class InputAnimalInfoActivity(var mainController: MainController) : BaseActivity
     }
 
     fun showAnimalCnt() {
-
+        if(animalList != null) {
+            println("현재 입력된 동물의 수 : ${animalList?.size}마리")
+        } else {
+            println("현재 입력된 동물의 수 : 0마리")
+        }
     }
 
     fun inputAnimalInfo() {
@@ -55,5 +71,20 @@ class InputAnimalInfoActivity(var mainController: MainController) : BaseActivity
             print("코의 길이 : ")
             noseLength = scanner.nextInt()
         }
+
+        animalModel = AnimalModel(type, name, age, furCnt, stripesCnt, noseLength)
+    }
+
+    fun saveAnimalInfo() {
+        // ArrayList가 null 이면 객체를 생성함
+        if(animalList == null){
+            animalList = ArrayList<AnimalModel>()
+        }
+
+        // 객체를 ArrayList에 담기
+        animalList?.add(animalModel)
+        // 파일에 저장
+        AnimalInfoDAO.saveAnimalInfoList(animalList!!)
+
     }
 }
--- 01_Kotlin/KotlinProject_ZooManager/src/com/lion/project2/dao/AnimalInfoDAO.kt
+++ 01_Kotlin/KotlinProject_ZooManager/src/com/lion/project2/dao/AnimalInfoDAO.kt
@@ -7,7 +7,7 @@ class AnimalInfoDAO {
 
     companion object {
         // 파일이 있는지 확인하는 메서드
-        fun isExistUserInfoFile() : Boolean {
+        fun isExistAnimalInfoFile() : Boolean {
             // File 객체 생성
             val saveFile = File("animalData.dat")
             // 파일이 존재하는지 확인
@@ -16,9 +16,9 @@ class AnimalInfoDAO {
         }
 
         // 파일에서 데이터를 읽어와 ArrayList에 담고 반환함
-        fun getStudentInfoList(): ArrayList<AnimalModel>? {
+        fun getAnimalInfoList(): ArrayList<AnimalModel>? {
             // 파일이 없다면 null 반환함
-            if (isExistUserInfoFile() == false) {
+            if (isExistAnimalInfoFile() == false) {
                 return null
             }
             // 파일에서 데이터를 읽어올 기본 스트림을 생성.
@@ -36,7 +36,7 @@ class AnimalInfoDAO {
         }
 
         // 데이터를 파일에 씀
-        fun saveStudentInfoList(dataList: ArrayList<AnimalModel>) {
+        fun saveAnimalInfoList(dataList: ArrayList<AnimalModel>) {
             // 파일에 데이터를 쓰기 위한 기본 스트림 생성
             // 쓰기를 할 때는 파일이 없을 경우 파일이 생성됨
             var fileOutputStream = FileOutputStream("animalData.dat")
