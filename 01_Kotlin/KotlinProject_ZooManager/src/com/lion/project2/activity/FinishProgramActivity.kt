9971a87e64d4c4436be167f8c1135678fcb348ad
rmadmsjm <dmswjdals12@naver.com>
2024-01-15 11:51:15 +0900

    feat: FinishProgramActivity

--- 01_Kotlin/KotlinProject_ZooManager/src/com/lion/project2/activity/FinishProgramActivity.kt
+++ 01_Kotlin/KotlinProject_ZooManager/src/com/lion/project2/activity/FinishProgramActivity.kt
@@ -1,5 +1,7 @@
 package com.lion.project2.activity
 
+import kotlin.system.exitProcess
+
 class FinishProgramActivity : BaseActivity() {
     override fun initActivity() {
     }
@@ -8,6 +10,9 @@ class FinishProgramActivity : BaseActivity() {
     }
 
     override fun showActivity() {
+        println()
+        println("프로그램이 종료되었습니다")
+        exitProcess(0)
     }
 
     override fun finishActivity() {
