1552e1ee13e544a646301dfff1ee10c8c370cd18
rmadmsjm <dmswjdals12@naver.com>
2024-01-19 10:21:17 +0900

    240119 EX05 설명

--- 02_Android/EX05/app/src/main/java/kr/co/lion/ex05/MainActivity.kt
+++ 02_Android/EX05/app/src/main/java/kr/co/lion/ex05/MainActivity.kt
@@ -58,5 +58,74 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
+
+
+        // 강사님 설명
+        setEvent()
+
+    }
+
+    // 강사님 설명
+    // 메서드를 나눠서 작업
+    // 각 뷰를 초기화 하는 메서드
+//    fun initView() {
+//
+//    }
+
+    // 이벤트를 설정하는 메서드
+    fun setEvent() {
+        activityMainBinding.apply {
+            // 더하기 버튼
+            buttonPlus.setOnClickListener {
+                // 입력한 값 가져오기
+                val number1 = editTextNumber1.text.toString().toInt()
+                val number2 = editTextNumber2.text.toString().toInt()
+
+                // 계산
+                val result = number1 + number2
+
+                // 출력
+                textViewResult.text = "결과 : $result"
+            }
+
+            // 빼기 버튼
+            buttonMinus.setOnClickListener {
+                // 입력한 값 가져오기
+                val number1 = editTextNumber1.text.toString().toInt()
+                val number2 = editTextNumber2.text.toString().toInt()
+
+                // 계산
+                val result = number1 - number2
+
+                // 출력
+                textViewResult.text = "결과 : $result"
+            }
+
+            // 곱하기 버튼
+            buttonMultiply.setOnClickListener {
+                // 입력한 값 가져오기
+                val number1 = editTextNumber1.text.toString().toInt()
+                val number2 = editTextNumber2.text.toString().toInt()
+
+                // 계산
+                val result = number1 * number2
+
+                // 출력
+                textViewResult.text = "결과 : $result"
+            }
+
+            // 나누기 버튼
+            buttonDivide.setOnClickListener {
+                // 입력한 값 가져오기
+                val number1 = editTextNumber1.text.toString().toInt()
+                val number2 = editTextNumber2.text.toString().toInt()
+
+                // 계산
+                val result = number1 / number2
+
+                // 출력
+                textViewResult.text = "결과 : $result"
+            }
+        }
     }
 }
