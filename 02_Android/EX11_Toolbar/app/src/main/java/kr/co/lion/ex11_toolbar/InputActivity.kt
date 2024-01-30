1c2fd3834bab3042c5d01a8372c6b839de62fe45
rmadmsjm <dmswjdals12@naver.com>
2024-01-30 09:40:54 +0900

    240130 EX11 설명

--- 02_Android/EX11_Toolbar/app/src/main/java/kr/co/lion/ex11_toolbar/InputActivity.kt
+++ 02_Android/EX11_Toolbar/app/src/main/java/kr/co/lion/ex11_toolbar/InputActivity.kt
@@ -23,6 +23,53 @@ class InputActivity : AppCompatActivity() {
     fun initData() {
     }
 
+    // 강사님 설명
+    // 툴바 설정
+    fun initToolbar(){
+        activityInputBinding.apply {
+            inputToolbar.apply {
+                title = "학생 정보 입력"
+
+                // 뒤로 가기
+                setNavigationIcon(R.drawable.arrow_back_24px)
+                setNavigationOnClickListener {
+                    setResult(RESULT_CANCELED)
+                    finish()
+                }
+
+                inflateMenu(R.menu.input_activity_menu)
+                setOnMenuItemClickListener{
+
+                    when(it.itemId){
+                        R.id.menuOk ->{
+                            // 입력한 정보를 객체에 담는다.
+                            val name = inputName.text.toString()
+                            val grade = inputGrade.text.toString().toInt()
+                            val kor = inputKor.text.toString().toInt()
+                            val eng = inputEng.text.toString().toInt()
+                            val math = inputMath.text.toString().toInt()
+
+                            val info1 = InfoClass(name, grade, kor, eng, math)
+
+                            // 데이터를 담을 Intent를 생성한다.
+                            val resultIntent = Intent()
+                            // 객체를 Intent에 저장할 때 writeToParcel 메서가 호출된다.
+                            resultIntent.putExtra("obj", info1)
+
+                            // 결과를 셋팅한다.
+                            setResult(RESULT_OK, resultIntent)
+
+                            // 현재 Activity를 종료한다.
+                            finish()
+                        }
+                    }
+
+                    true
+                }
+            }
+        }
+    }
+
     // View 초기 셋팅
     fun initView() {
         activityInputBinding.apply {
