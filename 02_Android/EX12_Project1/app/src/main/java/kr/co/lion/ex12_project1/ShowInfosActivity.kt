1754f664db904a1cc1e48b8908f239aca2fdf98d
rmadmsjm <dmswjdals12@naver.com>
2024-01-30 20:30:08 +0900

    240130 EX12 ShowInfosActivity

--- 02_Android/EX12_Project1/app/src/main/java/kr/co/lion/ex12_project1/ShowInfosActivity.kt
+++ 02_Android/EX12_Project1/app/src/main/java/kr/co/lion/ex12_project1/ShowInfosActivity.kt
@@ -1,5 +1,6 @@
 package kr.co.lion.ex12_project1
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kr.co.lion.ex12_project1.databinding.ActivityShowInfosBinding
@@ -19,6 +20,29 @@ class ShowInfosActivity : AppCompatActivity() {
     }
 
     fun initView() {
+        activityShowInfosBinding.apply {
+            textViewShowInfos.apply {
+                //Intent로부터 객체 추출
+                val info = if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                    intent.getParcelableExtra("obj", InfoClass::class.java)
+                } else {
+                    intent.getParcelableExtra<InfoClass>("obj")
+                }
+
+                text = "이름 : ${info?.name}\n"
+                append("학년 : ${info?.grade}학년\n")
+                append("\n")
+                append("국어 점수 : ${info?.kor}점\n")
+                append("영어 점수 : ${info?.eng}점\n")
+                append("수학 점수 : ${info?.math}점\n")
+                append("\n")
+
+                val total = info!!.kor + info!!.eng + info!!.math
+                append("총점 : ${total}점\n")
+                val avg = total / 3
+                append("평균 : ${avg}점")
+            }
+        }
     }
 
     fun initToolbar() {
