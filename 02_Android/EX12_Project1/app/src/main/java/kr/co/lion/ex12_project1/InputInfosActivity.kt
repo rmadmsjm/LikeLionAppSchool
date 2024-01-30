1e6db7f7b5d1b08970e6e10c9e61ead43bd5a876
rmadmsjm <dmswjdals12@naver.com>
2024-01-30 20:47:19 +0900

    240130 EX12 InputInfosActivity 다이얼로그, 스낵바

--- 02_Android/EX12_Project1/app/src/main/java/kr/co/lion/ex12_project1/InputInfosActivity.kt
+++ 02_Android/EX12_Project1/app/src/main/java/kr/co/lion/ex12_project1/InputInfosActivity.kt
@@ -3,6 +3,8 @@ package kr.co.lion.ex12_project1
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.snackbar.Snackbar
 import kr.co.lion.ex12_project1.databinding.ActivityInputInfosBinding
 
 class InputInfosActivity : AppCompatActivity() {
@@ -44,17 +46,33 @@ class InputInfosActivity : AppCompatActivity() {
                             val eng = textInputEng.text.toString().toInt()
                             val math = textInputMath.text.toString().toInt()
 
-                            val info = InfoClass(name, grade, kor, eng, math)
+                            if(name == null || grade == null || kor == null || eng == null || math == null) {
+                                // 다이얼로그
+                                val builder = MaterialAlertDialogBuilder(this@InputInfosActivity).apply {
+                                    // 타이틀
+                                    setTitle("⚠️ 주의 ⚠️")
+                                    // 메시지
+                                    setMessage("입력값을 모두 입력해주세요")
+                                }
 
-                            // 데이터를 담을 Intent를 생성
-                            val resultIntent = Intent()
-                            // 객체를 Intent에 저장할 때 writeToParcel 메서가 호출됨
-                            resultIntent.putExtra("obj", info)
+                                // 다이얼로그 띄우기
+                                builder.show()
+                            } else {
+                                val info = InfoClass(name, grade, kor, eng, math)
 
-                            // 결과 세팅
-                            setResult(RESULT_OK, resultIntent)
+                                // 데이터를 담을 Intent를 생성
+                                val resultIntent = Intent()
+                                // 객체를 Intent에 저장할 때 writeToParcel 메서가 호출됨
+                                resultIntent.putExtra("obj", info)
 
-                            finish()
+                                // 결과 세팅
+                                setResult(RESULT_OK, resultIntent)
+
+                                // 스낵바
+                                Snackbar.make(activityInputInfosBinding.root, "Your message", Snackbar.LENGTH_SHORT).show()
+
+                                finish()
+                            }
                         }
                     }
                     true
