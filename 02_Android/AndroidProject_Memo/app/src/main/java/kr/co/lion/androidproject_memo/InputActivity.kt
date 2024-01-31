08a83d7e0877536cae1557ebc1561c0ff9762160
rmadmsjm <dmswjdals12@naver.com>
2024-01-31 16:49:19 +0900

    feat(InputActivity): Toolbar 설정

--- 02_Android/AndroidProject_Memo/app/src/main/java/kr/co/lion/androidproject_memo/InputActivity.kt
+++ 02_Android/AndroidProject_Memo/app/src/main/java/kr/co/lion/androidproject_memo/InputActivity.kt
@@ -12,5 +12,38 @@ class InputActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         activityInputBinding = ActivityInputBinding.inflate(layoutInflater)
         setContentView(activityInputBinding.root)
+
+        setToolbar()
+    }
+
+    // Toolbar
+    fun setToolbar() {
+        activityInputBinding.apply {
+            toolbarInput.apply {
+                // 타이틀
+                title = "메모 작성"
+
+                // back button
+                setNavigationIcon(R.drawable.arrow_back_24px)
+                setNavigationOnClickListener {
+                    setResult(RESULT_CANCELED)
+                    finish()
+                }
+
+                // 메뉴 설정
+                inflateMenu(R.menu.menu_input)
+                // 메뉴 리스너
+                setOnMenuItemClickListener {
+                    // 메뉴 id로 분기
+                    when(it.itemId) {
+                        R.id.menuItemInputSubmit -> {
+                            setResult(RESULT_OK)
+                            finish()
+                        }
+                    }
+                    true
+                }
+            }
+        }
     }
 }
