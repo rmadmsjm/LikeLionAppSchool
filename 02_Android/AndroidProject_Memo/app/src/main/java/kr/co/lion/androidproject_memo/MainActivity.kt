433c38337d2c521763ca313b5f8ef17d9d971a06
rmadmsjm <dmswjdals12@naver.com>
2024-01-31 16:40:36 +0900

    feat(MainActivity): Toolbar 및 Launcher 설정

--- 02_Android/AndroidProject_Memo/app/src/main/java/kr/co/lion/androidproject_memo/MainActivity.kt
+++ 02_Android/AndroidProject_Memo/app/src/main/java/kr/co/lion/androidproject_memo/MainActivity.kt
@@ -1,16 +1,55 @@
 package kr.co.lion.androidproject_memo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 import kr.co.lion.androidproject_memo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var activityMainBinding: ActivityMainBinding
 
+    // InputActivity Launcher
+    lateinit var inputactivityLauncher: ActivityResultLauncher<Intent>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(activityMainBinding.root)
+    }
+
+    //
+    fun initData() {
+        // InputActivity Launcher
+        val inputContract = ActivityResultContracts.StartActivityForResult()
+        inputactivityLauncher = registerForActivityResult(inputContract) {
+        }
+    }
+
+    // Toolbar
+    fun setToolbar() {
+        activityMainBinding.apply {
+            toolbarMain.apply {
+                // 타이틀
+                title = "메모 관리"
+                // 메뉴 설정
+                inflateMenu(R.menu.menu_main)
+                // 메뉴 리스너
+                setOnMenuItemClickListener {
+                    // 메뉴 id로 분기
+                    when(it.itemId) {
+                        R.id.menuItemMainAdd -> {
+                            val mainIntent = Intent(this@MainActivity, InputActivity::class.java)
+                            inputactivityLauncher.launch(mainIntent)
+                        }
+                    }
+
+                    true
+                }
+            }
+        }
     }
 }
