a20de0777f80169980c3839e00aea771ef423bd1
rmadmsjm <dmswjdals12@naver.com>
2024-01-31 17:32:27 +0900

    feat(MainActivity): RecyclerVeiw 설정

--- 02_Android/AndroidProject_Memo/app/src/main/java/kr/co/lion/androidproject_memo/MainActivity.kt
+++ 02_Android/AndroidProject_Memo/app/src/main/java/kr/co/lion/androidproject_memo/MainActivity.kt
@@ -3,10 +3,17 @@ package kr.co.lion.androidproject_memo
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContract
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kr.co.lion.androidproject_memo.databinding.ActivityMainBinding
+import kr.co.lion.androidproject_memo.databinding.RowRecyclerviewBinding
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,7 +31,7 @@ class MainActivity : AppCompatActivity() {
         setToolbar()
     }
 
-    //
+    // 기본 데이터 및 객체 세팅
     fun initData() {
         // InputActivity Launcher
         val inputContract = ActivityResultContracts.StartActivityForResult()
@@ -53,6 +60,55 @@ class MainActivity : AppCompatActivity() {
                     true
                 }
             }
+        }
+    }
+
+    // View 구성
+    fun setView() {
+        activityMainBinding.apply {
+            recyclerViewMain.apply {
+                adapter = RecyclerViewAdapter()
+                layoutManager = LinearLayoutManager(this@MainActivity)
+            }
+        }
+    }
+
+    // RecycelrView Adapter
+    inner class RecyclerViewAdapter : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolderClass>() {
+        // ViewHolder
+        inner class ViewHolderClass(rowRecyclerviewBinding: RowRecyclerviewBinding)
+            : RecyclerView.ViewHolder(rowRecyclerviewBinding.root) {
+                val rowRecyclerviewBinding: RowRecyclerviewBinding
+
+                init {
+                    this.rowRecyclerviewBinding = rowRecyclerviewBinding
+
+                    // 항목의 가로, 세로 길이 설정
+                    this.rowRecyclerviewBinding.root.layoutParams = ViewGroup.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT
+                    )
+                }
+            }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderClass {
+            val rowRecyclerviewBinding = RowRecyclerviewBinding.inflate(layoutInflater)
+            val viewHolderClass = ViewHolderClass(rowRecyclerviewBinding)
+
+            return viewHolderClass
+        }
+
+        override fun getItemCount(): Int {
+            return 20
+        }
+
+        override fun onBindViewHolder(holder: ViewHolderClass, position: Int) {
+            holder.rowRecyclerviewBinding.textViewRowTitle.text = "제목 $position"
+
+            // 현재 날짜 설정
+            val dateFormat = SimpleDateFormat("yyyy.MM,dd", Locale.getDefault())
+            val currentDate = dateFormat.format(Date(System.currentTimeMillis()))
+            holder.rowRecyclerviewBinding.textViewRowDate.text = currentDate
         }
     }
 }
