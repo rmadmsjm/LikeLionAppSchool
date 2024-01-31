8e54f738bab16ab79c4ebf02cc83012557761ca2
rmadmsjm <dmswjdals12@naver.com>
2024-01-31 17:03:35 +0900

    feat(EditActivity): Toolbar 설정

--- 02_Android/AndroidProject_Memo/app/src/main/java/kr/co/lion/androidproject_memo/EditActivity.kt
+++ 02_Android/AndroidProject_Memo/app/src/main/java/kr/co/lion/androidproject_memo/EditActivity.kt
@@ -13,4 +13,36 @@ class EditActivity : AppCompatActivity() {
         activityEditBinding = ActivityEditBinding.inflate(layoutInflater)
         setContentView(activityEditBinding.root)
     }
+
+    // Toolbar
+    fun setToolbar() {
+        activityEditBinding.apply {
+            toolbarEdit.apply {
+                // 타이틀
+                title = "메모 수정"
+
+                // back button
+                setNavigationIcon(R.drawable.arrow_back_24px)
+                setNavigationOnClickListener {
+                    setResult(RESULT_CANCELED)
+                    finish()
+                }
+
+                // 메뉴 설정
+                inflateMenu(R.menu.menu_edit)
+                // 메뉴 리스너
+                setOnMenuItemClickListener {
+                    // 메뉴 id로 분기
+                    when(it.itemId) {
+                        R.id.menuItemEditSubmit -> {
+                            setResult(RESULT_OK)
+                            finish()
+                        }
+                    }
+
+                    true
+                }
+            }
+        }
+    }
 }
