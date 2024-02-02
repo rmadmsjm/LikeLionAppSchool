15f2aef8d33f2caf1e51fff87d4cea4db59c8fc4
rmadmsjm <dmswjdals12@naver.com>
2024-02-02 11:32:35 +0900

    feat(MainActivity) : RecyclerView 설정

--- 02_Android/AndroidProject1/app/src/main/java/kr/co/lion/androidproject1/MainActivity.kt
+++ 02_Android/AndroidProject1/app/src/main/java/kr/co/lion/androidproject1/MainActivity.kt
@@ -2,7 +2,12 @@ package kr.co.lion.androidproject1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.divider.MaterialDividerItemDecoration
 import kr.co.lion.androidproject1.databinding.ActivityMainBinding
+import kr.co.lion.androidproject1.databinding.RowMainBinding
 
 class MainActivity : AppCompatActivity() {
 
@@ -33,6 +38,51 @@ class MainActivity : AppCompatActivity() {
                     true
                 }
             }
+        }
+    }
+
+    // 뷰 설정
+    fun setView() {
+        activityMainBinding.apply {
+            recyclerViewMain.apply {
+                adapter = RecyclerViewAdapter()
+                layoutManager = LinearLayoutManager(this@MainActivity)
+                val deco = MaterialDividerItemDecoration(this@MainActivity, MaterialDividerItemDecoration.VERTICAL)
+                addItemDecoration(deco)
+            }
+        }
+    }
+
+    // RecyclerView
+    inner class RecyclerViewAdapter : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolderClass>() {
+        inner class ViewHolderClass(rowMainBinding: RowMainBinding) : RecyclerView.ViewHolder(rowMainBinding.root) {
+            val rowMainBinding : RowMainBinding
+
+            init {
+                this.rowMainBinding = rowMainBinding
+
+                // 가로 세로 길이 설정
+                this.rowMainBinding.root.layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT
+                )
+            }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderClass {
+            val rowMainBinding = RowMainBinding.inflate(layoutInflater)
+            val viewHolderClass = ViewHolderClass(rowMainBinding)
+
+            return viewHolderClass
+        }
+
+        override fun getItemCount(): Int {
+            return 20
+        }
+
+        override fun onBindViewHolder(holder: ViewHolderClass, position: Int) {
+            holder.rowMainBinding.textViewRowMainName.text = "이름 $position"
+            holder.rowMainBinding.textViewRowMainType.text = "종류 $position"
         }
     }
 }
