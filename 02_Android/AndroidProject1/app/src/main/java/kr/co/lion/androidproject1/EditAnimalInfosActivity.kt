523f20a18f236031a83bcc11b0b11070ff719ca5
rmadmsjm <dmswjdals12@naver.com>
2024-02-02 11:03:30 +0900

    feat(EditAnimalInfosActivity) : toolbar 설정

--- 02_Android/AndroidProject1/app/src/main/java/kr/co/lion/androidproject1/EditAnimalInfosActivity.kt
+++ 02_Android/AndroidProject1/app/src/main/java/kr/co/lion/androidproject1/EditAnimalInfosActivity.kt
@@ -13,4 +13,32 @@ class EditAnimalInfosActivity : AppCompatActivity() {
         activityEditAnimalInfosBinding = ActivityEditAnimalInfosBinding.inflate(layoutInflater)
         setContentView(activityEditAnimalInfosBinding.root)
     }
+
+    // 툴바
+    fun setToolbar() {
+        activityEditAnimalInfosBinding.apply {
+            toolbarAnimalEditInfos.apply {
+                title = "동물 정보 수정"
+
+                // back
+                setNavigationIcon(R.drawable.arrow_back_24px)
+                setNavigationOnClickListener {
+                    setResult(RESULT_CANCELED)
+                    finish()
+                }
+
+                // 메뉴
+                inflateMenu(R.menu.menu_edit)
+                setOnMenuItemClickListener {
+                    // id로 분기
+                    when(it.itemId) {
+                        R.id.menuItemEditOk -> {
+                        }
+                    }
+                    
+                    true
+                }
+            }
+        }
+    }
 }
