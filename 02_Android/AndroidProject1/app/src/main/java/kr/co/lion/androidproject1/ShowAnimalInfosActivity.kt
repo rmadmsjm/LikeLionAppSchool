cb0514ae955935b5cac3eefb8cd532f123f023e5
rmadmsjm <dmswjdals12@naver.com>
2024-02-02 10:41:01 +0900

    feat(ShowAnimalInfosActivity) : toolbar 설정

--- 02_Android/AndroidProject1/app/src/main/java/kr/co/lion/androidproject1/ShowAnimalInfosActivity.kt
+++ 02_Android/AndroidProject1/app/src/main/java/kr/co/lion/androidproject1/ShowAnimalInfosActivity.kt
@@ -13,4 +13,33 @@ class ShowAnimalInfosActivity : AppCompatActivity() {
         activityShowAnimalInfosBinding = ActivityShowAnimalInfosBinding.inflate(layoutInflater)
         setContentView(activityShowAnimalInfosBinding.root)
     }
+
+    // 툴바
+    fun setToolbar() {
+        activityShowAnimalInfosBinding.apply {
+            toolbarShowAnimalInfos.apply {
+                title = "동물 정보"
+
+                // back
+                setNavigationIcon(R.drawable.arrow_back_24px)
+                setNavigationOnClickListener {
+                    setResult(RESULT_CANCELED)
+                    finish()
+                }
+
+                // 메뉴
+                inflateMenu(R.menu.menu_show)
+                setOnMenuItemClickListener {
+                    // id로 분기
+                    when(it.itemId) {
+                        R.id.menuItemShowEdit -> {
+                        }
+                        R.id.menuItemShowDelete -> {
+                        }
+                    }
+                    true
+                }
+            }
+        }
+    }
 }
