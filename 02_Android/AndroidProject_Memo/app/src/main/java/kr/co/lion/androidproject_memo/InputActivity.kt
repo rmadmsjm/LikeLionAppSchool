aa93f0833a34aa987ce0da2ec6dbfc5b1ead1d65
rmadmsjm <dmswjdals12@naver.com>
2024-02-02 14:41:57 +0900

    feat(InputActivity, activity_input): textField 포커스, 키보드 올리기

--- 02_Android/AndroidProject_Memo/app/src/main/java/kr/co/lion/androidproject_memo/InputActivity.kt
+++ 02_Android/AndroidProject_Memo/app/src/main/java/kr/co/lion/androidproject_memo/InputActivity.kt
@@ -2,7 +2,11 @@ package kr.co.lion.androidproject_memo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
+import android.view.inputmethod.InputMethodManager
+import com.google.android.material.textfield.TextInputEditText
 import kr.co.lion.androidproject_memo.databinding.ActivityInputBinding
+import kotlin.concurrent.thread
 
 class InputActivity : AppCompatActivity() {
 
@@ -14,6 +18,7 @@ class InputActivity : AppCompatActivity() {
         setContentView(activityInputBinding.root)
 
         setToolbar()
+        setView()
     }
 
     // Toolbar
@@ -44,6 +49,34 @@ class InputActivity : AppCompatActivity() {
                     true
                 }
             }
+        }
+    }
+
+    // View 설정
+    fun setView() {
+        activityInputBinding.apply {
+            // 메모 제목 textField 포커스
+            textFieldInputTitle.requestFocus()
+
+            // 키보드 올리기
+            showSoftInput(textFieldInputTitle)
+
+            // 메모 내용 textField 엔터키 클릭 시 입력 완료 처리
+            textFieldInputContext.setOnEditorActionListener { v, actionId, event ->
+                //
+                true
+            }
+        }
+    }
+
+    // 입력 완료 처리 메서드
+
+    // 키보드 올리는 메서드
+    fun showSoftInput(focusView: TextInputEditText) {
+        thread {
+            SystemClock.sleep(200)
+            val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.showSoftInput(focusView, 0)
         }
     }
 }
