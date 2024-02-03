8923c29771681b2531e396b7c85b50ed0742d098
rmadmsjm <dmswjdals12@naver.com>
2024-02-03 20:55:16 +0900

    feat(ShowActivity): memoData 받아오기

--- 02_Android/AndroidProject_Memo/app/src/main/java/kr/co/lion/androidproject_memo/ShowActivity.kt
+++ 02_Android/AndroidProject_Memo/app/src/main/java/kr/co/lion/androidproject_memo/ShowActivity.kt
@@ -1,6 +1,7 @@
 package kr.co.lion.androidproject_memo
 
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.result.ActivityResultLauncher
@@ -9,15 +10,15 @@ import kr.co.lion.androidproject_memo.databinding.ActivityShowBinding
 
 class ShowActivity : AppCompatActivity() {
 
-    lateinit var acitivityShowBinding: ActivityShowBinding
+    lateinit var activityShowBinding: ActivityShowBinding
 
     // EditActivity Launcher
     lateinit var editActivityLauncher: ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        acitivityShowBinding = ActivityShowBinding.inflate(layoutInflater)
-        setContentView(acitivityShowBinding.root)
+        activityShowBinding = ActivityShowBinding.inflate(layoutInflater)
+        setContentView(activityShowBinding.root)
 
         initData()
         setToolbar()
@@ -33,7 +34,7 @@ class ShowActivity : AppCompatActivity() {
 
     // Toolbar
     fun setToolbar() {
-        acitivityShowBinding.apply {
+        activityShowBinding.apply {
             toolbarShow.apply {
                 // 타이틀
                 title = "메모 보기"
@@ -61,6 +62,31 @@ class ShowActivity : AppCompatActivity() {
 
                     true
                 }
+            }
+        }
+    }
+
+    // View 설정
+    fun setView() {
+        activityShowBinding.apply {
+            // Intent로부터 메모 데이터 객체 추출
+            val memoData = if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                intent.getParcelableExtra("memoData", MemoClass::class.java)
+            } else {
+                intent.getParcelableExtra<MemoClass>("memoData")
+            }
+
+            // textViewShowTitle
+            textViewShowTitle.apply {
+                text = memoData?.title
+            }
+            // textViewShowDate
+            textViewShowDate.apply {
+                text = memoData?.date
+            }
+            // textViewShowDate
+            textViewShowDate.apply {
+                text = memoData?.context
             }
         }
     }
