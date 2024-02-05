f9d580c8096116299e528d08d3cb2499f61871af
rmadmsjm <dmswjdals12@naver.com>
2024-02-05 10:47:51 +0900

    240205 InputActivity toolbar 설정

--- 02_Android/AndroidProject1Test/app/src/main/java/kr/co/lion/androidproject1test/InputActivity.kt
+++ 02_Android/AndroidProject1Test/app/src/main/java/kr/co/lion/androidproject1test/InputActivity.kt
@@ -12,5 +12,25 @@ class InputActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         activityInputBinding = ActivityInputBinding.inflate(layoutInflater)
         setContentView(activityInputBinding.root)
+
+        setToolbar()
+    }
+
+    // 툴바
+    fun setToolbar() {
+        activityInputBinding.apply {
+            toolbarInput.apply {
+                // 타이틀
+                title = "동물 등록"
+                // back button
+                setNavigationIcon(R.drawable.arrow_back_24px)
+                setNavigationOnClickListener {
+                    setResult(RESULT_CANCELED)
+                    finish()
+                }
+                // 메뉴
+                inflateMenu(R.menu.menu_input)
+            }
+        }
     }
 }
