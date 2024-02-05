37f644e2dc2dc4b3474dc1dc501d262359ec3672
rmadmsjm <dmswjdals12@naver.com>
2024-02-05 11:21:27 +0900

    240205 ShowActivity toolbar 설정

--- 02_Android/AndroidProject1Test/app/src/main/java/kr/co/lion/androidproject1test/ShowActivity.kt
+++ 02_Android/AndroidProject1Test/app/src/main/java/kr/co/lion/androidproject1test/ShowActivity.kt
@@ -2,10 +2,37 @@ package kr.co.lion.androidproject1test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.co.lion.androidproject1test.databinding.ActivityShowBinding
 
 class ShowActivity : AppCompatActivity() {
+
+    lateinit var activityShowBinding: ActivityShowBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_show)
+        activityShowBinding = ActivityShowBinding.inflate(layoutInflater)
+        setContentView(activityShowBinding.root)
+
+        setToolbar()
+    }
+
+    // 툴바
+    fun setToolbar() {
+        activityShowBinding.apply {
+            toolbarShow.apply {
+                // 타이틀
+                title = "동물 정보"
+
+                // back button
+                setNavigationIcon(R.drawable.arrow_back_24px)
+                setNavigationOnClickListener {
+                    setResult(RESULT_CANCELED)
+                    finish()
+                }
+
+                // 메뉴
+                inflateMenu(R.menu.menu_show)
+            }
+        }
     }
 }
