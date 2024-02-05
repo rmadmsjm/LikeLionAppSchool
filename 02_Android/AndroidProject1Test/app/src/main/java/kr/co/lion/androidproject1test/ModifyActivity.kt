50ccec785d4b46d110d4389219b919d8c85e96e5
rmadmsjm <dmswjdals12@naver.com>
2024-02-05 13:30:05 +0900

    240205 ModifyActivity, activity_modify

--- 02_Android/AndroidProject1Test/app/src/main/java/kr/co/lion/androidproject1test/ModifyActivity.kt
+++ 02_Android/AndroidProject1Test/app/src/main/java/kr/co/lion/androidproject1test/ModifyActivity.kt
@@ -2,10 +2,37 @@ package kr.co.lion.androidproject1test
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.co.lion.androidproject1test.databinding.ActivityModifyBinding
 
 class ModifyActivity : AppCompatActivity() {
+
+    lateinit var activityModifyBinding: ActivityModifyBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_modify)
+        activityModifyBinding = ActivityModifyBinding.inflate(layoutInflater)
+        setContentView(activityModifyBinding.root)
+
+        setToolbar()
+    }
+
+    // 툴바
+    fun setToolbar() {
+        activityModifyBinding.apply {
+            toolbarModify.apply {
+                // 타이틀
+                title = "동물 정보 수정"
+
+                // back button
+                setNavigationIcon(R.drawable.arrow_back_24px)
+                setNavigationOnClickListener {
+                    setResult(RESULT_CANCELED)
+                    finish()
+                }
+
+                // 메뉴
+                inflateMenu(R.menu.menu_modify)
+            }
+        }
     }
 }
