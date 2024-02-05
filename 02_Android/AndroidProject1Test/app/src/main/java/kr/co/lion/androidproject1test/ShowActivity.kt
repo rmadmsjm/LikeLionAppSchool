29cf0330c81b9b4a72906a438528393e4c317c05
rmadmsjm <dmswjdals12@naver.com>
2024-02-05 11:34:21 +0900

    240205 ShowActivity, activity_show

--- 02_Android/AndroidProject1Test/app/src/main/java/kr/co/lion/androidproject1test/ShowActivity.kt
+++ 02_Android/AndroidProject1Test/app/src/main/java/kr/co/lion/androidproject1test/ShowActivity.kt
@@ -14,6 +14,7 @@ class ShowActivity : AppCompatActivity() {
         setContentView(activityShowBinding.root)
 
         setToolbar()
+        setView()
     }
 
     // 툴바
@@ -32,6 +33,31 @@ class ShowActivity : AppCompatActivity() {
 
                 // 메뉴
                 inflateMenu(R.menu.menu_show)
+            }
+        }
+    }
+
+    // View 설정
+    fun setView() {
+        activityShowBinding.apply {
+            // textView
+            textViewShowInfo.apply {
+                // 공통
+                text = "동물 종류 : \n"
+                append("이름 : \n")
+                append("나이 : 살\n")
+
+                // 사자
+                append("털의 개수 : 개\n")
+                append("성별 : \n")
+
+                // 호랑이
+                append("줄무늬 개수 : 개\n")
+                append("몸무게 : kg\n")
+
+                // 기린
+                append("목의 길이 : cm\n")
+                append("달리는 속도 : km/h\n")
             }
         }
     }
