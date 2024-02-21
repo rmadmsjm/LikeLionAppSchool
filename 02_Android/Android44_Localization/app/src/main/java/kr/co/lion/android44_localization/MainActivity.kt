b4b6d86221015cf4d707678aa849506bd5331eb9
rmadmsjm <dmswjdals12@naver.com>
2024-02-21 13:12:15 +0900

    240221 Localization

---
+++ 02_Android/Android44_Localization/app/src/main/java/kr/co/lion/android44_localization/MainActivity.kt
@@ -0,0 +1,20 @@
+package kr.co.lion.android44_localization
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.co.lion.android44_localization.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var activityMainBinding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
+        activityMainBinding.apply {
+            textView2.setText(R.string.greeting)
+            imageView2.setImageResource(R.drawable.flag1)
+        }
+    }
+}
