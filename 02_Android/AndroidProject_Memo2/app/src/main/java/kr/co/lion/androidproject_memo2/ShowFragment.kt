a48eb21c69b9fa8940ce02bfe44f9796feb3c031
rmadmsjm <dmswjdals12@naver.com>
2024-02-22 16:38:17 +0900

    Feat: ShowFragment

---
+++ 02_Android/AndroidProject_Memo2/app/src/main/java/kr/co/lion/androidproject_memo2/ShowFragment.kt
@@ -0,0 +1,46 @@
+package kr.co.lion.androidproject_memo2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kr.co.lion.androidproject_memo2.databinding.FragmentShowBinding
+
+class ShowFragment : Fragment() {
+
+    lateinit var fragmentShowBinding: FragmentShowBinding
+    lateinit var mainActivity: MainActivity
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        fragmentShowBinding = FragmentShowBinding.inflate(inflater)
+        mainActivity = activity as MainActivity
+
+        settingToolbar()
+
+        return fragmentShowBinding.root
+    }
+
+    // 툴바
+    fun settingToolbar() {
+        fragmentShowBinding.apply {
+            toolbarShow.apply {
+                // 타이틀
+                title = "메모 보기"
+
+                // back
+                setNavigationIcon(R.drawable.arrow_back_24px)
+                setNavigationOnClickListener {
+                    mainActivity.removeFragment(FragmentName.SHOW_FRAGMENT)
+                }
+
+                // 메뉴
+                inflateMenu(R.menu.menu_show)
+                // 메뉴 리스너
+                setOnMenuItemClickListener {
+                    true
+                }
+            }
+        }
+    }
+}
