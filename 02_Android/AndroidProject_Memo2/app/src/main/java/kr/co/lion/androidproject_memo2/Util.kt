1091401b27dd2c80cec5bec2a9e0ece979dd7326
rmadmsjm <dmswjdals12@naver.com>
2024-02-23 18:07:01 +0900

    Feat: Util

---
+++ 02_Android/AndroidProject_Memo2/app/src/main/java/kr/co/lion/androidproject_memo2/Util.kt
@@ -0,0 +1,30 @@
+package kr.co.lion.androidproject_memo2
+
+import android.content.Context
+import android.os.SystemClock
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.concurrent.thread
+
+class Util {
+
+    companion object {
+        // 키보드 올리는 메서드
+        fun showSoftInput(context: Context, view: View) {
+            // 포커스 주기
+            view.requestFocus()
+
+            thread {
+                SystemClock.sleep(500)
+                val inputMethodManager = context.getSystemService(AppCompatActivity.INPUT_METHOD_SERVICE) as InputMethodManager
+                inputMethodManager.showSoftInput(view, 0)
+            }
+        }
+    }
+}
+
+enum class ResultCode(var codeNum:Int) {
+    RESULT_EDIT(2),
+    RESULT_DELETE(3)
+}
