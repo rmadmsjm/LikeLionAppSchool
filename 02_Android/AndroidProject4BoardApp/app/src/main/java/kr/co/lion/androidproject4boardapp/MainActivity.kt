a4d35717b845fc3c5ac02dc9d7b290bb90f61af5
rmadmsjm <dmswjdals12@naver.com>
2024-03-04 11:23:23 +0900

    240304 Splash Screen

--- 02_Android/AndroidProject4BoardApp/app/src/main/java/kr/co/lion/androidproject4boardapp/MainActivity.kt
+++ 02_Android/AndroidProject4BoardApp/app/src/main/java/kr/co/lion/androidproject4boardapp/MainActivity.kt
@@ -2,10 +2,21 @@ package kr.co.lion.androidproject4boardapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import kr.co.lion.androidproject4boardapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var activityMainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // 스클래시 스크린 나타나게 하기
+        // 반드시 setContnetView 전에 호출되어야 함
+        installSplashScreen()
+
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
     }
 }
