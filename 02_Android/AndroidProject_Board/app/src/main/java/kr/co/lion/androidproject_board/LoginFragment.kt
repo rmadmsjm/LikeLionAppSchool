5a083876857390355003edd9da95e2209e3a3158
rmadmsjm <dmswjdals12@naver.com>
2024-03-04 17:26:41 +0900

    Feat: LoginFragment 입력 데이터 유효성 검사

--- 02_Android/AndroidProject_Board/app/src/main/java/kr/co/lion/androidproject_board/LoginFragment.kt
+++ 02_Android/AndroidProject_Board/app/src/main/java/kr/co/lion/androidproject_board/LoginFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
+import com.google.android.material.snackbar.Snackbar
 import kr.co.lion.androidproject_board.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
@@ -18,7 +20,9 @@ class LoginFragment : Fragment() {
         mainActivity = activity as MainActivity
 
         settingToolbar()
-        settingView()
+        settingTextField()
+        settingButtonLoginSignIn()
+        settingButtonLoginSignUp()
 
         return fragmentLoginBinding.root
     }
@@ -32,12 +36,72 @@ class LoginFragment : Fragment() {
         }
     }
 
-    // View 설정
-    fun settingView() {
+    // 로그인 Button 설정
+    fun settingButtonLoginSignIn() {
+        fragmentLoginBinding.apply {
+            buttonLoginSignIn.setOnClickListener {
+                val chk = checkTextField()
+
+                if(chk == true) {
+                    // 키보드 내리기
+                    mainActivity.hideSoftInput()
+
+                    Snackbar.make(fragmentLoginBinding.root, "로그인 되었습니다", Snackbar.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    // 회원가입 Button 설정
+    fun settingButtonLoginSignUp() {
         fragmentLoginBinding.apply {
             buttonLoginSignUp.setOnClickListener {
                 mainActivity.replaceFragment(FragmentName.JOIN_FRAGMENT, true, true, null)
             }
+        }
+    }
+
+    // 입력 요소 설정
+    fun settingTextField() {
+        fragmentLoginBinding.apply {
+            // 에러 메시지가 보여지는 상황에서 값을 입력했을 때 에러 메세지 없애기
+            textFieldLoginId.addTextChangedListener {
+                textInputLayoutLoginId.isErrorEnabled = false
+            }
+            textFieldLoginPassword.addTextChangedListener {
+                textInputLayoutLoginPassword.isErrorEnabled = false
+            }
+        }
+    }
+
+    // 유효성 검사
+    fun checkTextField() : Boolean {
+        fragmentLoginBinding.apply {
+            var emptyView: View? = null
+
+            // 아이디
+            if(textFieldLoginId.text.toString().trim().isEmpty()) {
+                textInputLayoutLoginId.error = "아이디를 입력해주세요"
+
+                if(emptyView == null) {
+                    emptyView = textFieldLoginId
+                }
+            }
+
+            // 비밀번호
+            if(textFieldLoginPassword.text.toString().trim().isEmpty()) {
+                textInputLayoutLoginPassword.error = "비밀번호를 입력해주세요"
+
+                if(emptyView == null) {
+                    emptyView = textInputLayoutLoginPassword
+                }
+            }
+
+            if(emptyView != null) {
+                return false
+            }
+
+            return true
         }
     }
 }
