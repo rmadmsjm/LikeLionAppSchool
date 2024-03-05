a4cf9d759111057c03007d1054b4a2969d34ef3c
rmadmsjm <dmswjdals12@naver.com>
2024-03-05 09:28:48 +0900

    240305 MainActivity 기본 설정

---
+++ 02_Android/AndroidProject4BoardApp/app/src/main/java/kr/co/lion/androidproject4boardapp/Tools.kt
@@ -0,0 +1,12 @@
+package kr.co.lion.androidproject4boardapp
+
+class Tools {
+    companion object {
+    }
+}
+
+// MainActivity에 보여질 Fragment의 이름
+enum class MainFragmentName(var str: String) {
+    A_FRAGMENT("A"),
+    B_FRAGMENT("B")
+}
