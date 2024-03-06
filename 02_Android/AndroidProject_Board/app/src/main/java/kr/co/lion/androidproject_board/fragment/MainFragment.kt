f4fea4664afe5b8087828298eb67411c468facc0
rmadmsjm <dmswjdals12@naver.com>
2024-03-06 17:30:24 +0900

    Feat: MainFragment SearchBar, SearchView 설정

--- 02_Android/AndroidProject_Board/app/src/main/java/kr/co/lion/androidproject_board/fragment/MainFragment.kt
+++ 02_Android/AndroidProject_Board/app/src/main/java/kr/co/lion/androidproject_board/fragment/MainFragment.kt
@@ -5,10 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kr.co.lion.androidproject_board.ContentActivity
 import kr.co.lion.androidproject_board.ContentFragmentName
 import kr.co.lion.androidproject_board.R
 import kr.co.lion.androidproject_board.databinding.FragmentMainBinding
+import kr.co.lion.androidproject_board.databinding.RowMainBinding
 
 class MainFragment : Fragment() {
 
@@ -21,6 +24,9 @@ class MainFragment : Fragment() {
         contentActivity = activity as ContentActivity
 
         settingToolbar()
+        settingSearchBar()
+        settingRecyclerViewMain()
+        settingRecyclerViewMainSearch()
 
         return fragmentMainBinding.root
     }
@@ -36,6 +42,15 @@ class MainFragment : Fragment() {
                     // NavigationDrawer
                     contentActivity.activityContentBinding.drawerLayoutContent.open()
                 }
+            }
+        }
+    }
+
+    // searchBar 설정
+    fun settingSearchBar() {
+        fragmentMainBinding.apply {
+            searchBarMain.apply {
+                hint = "여기를 눌러 검색해주세요"
 
                 inflateMenu(R.menu.menu_main)
                 setOnMenuItemClickListener {
@@ -47,6 +62,94 @@ class MainFragment : Fragment() {
                     true
                 }
             }
+
+            searchViewMain.apply {
+                hint = "검색어를 입력해주세요"
+            }
+        }
+    }
+
+    // 메인 화면 RecyclerView 설정
+    fun settingRecyclerViewMain() {
+        fragmentMainBinding.apply {
+            recyclerViewMain.apply {
+                adapter = MainRecyclerViewAdapter()
+                layoutManager = LinearLayoutManager(contentActivity)
+            }
+        }
+    }
+
+    // 검색 화면 RecyclerView 설정
+    fun settingRecyclerViewMainSearch() {
+        fragmentMainBinding.apply {
+            recyclerViewMainSearch.apply {
+                adapter = SearchRecyclerViewAdapter()
+                layoutManager = LinearLayoutManager(contentActivity)
+            }
+        }
+    }
+
+    // 메인 화면 RecyclerView Adapter
+    inner class MainRecyclerViewAdapter() : RecyclerView.Adapter<MainRecyclerViewAdapter.MainViewHolder>() {
+        inner class MainViewHolder(rowMainBinding: RowMainBinding) : RecyclerView.ViewHolder(rowMainBinding.root) {
+            val rowMainBinding: RowMainBinding
+
+            init {
+                this.rowMainBinding = rowMainBinding
+
+                this.rowMainBinding.root.layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT
+                )
+            }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
+            val rowMainBinding = RowMainBinding.inflate(layoutInflater)
+            val mainViewHolder = MainViewHolder(rowMainBinding)
+
+            return mainViewHolder
+        }
+
+        override fun getItemCount(): Int {
+            return 10
+        }
+
+        override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
+            holder.rowMainBinding.textViewRowMainTitle.text = "제목 $position"
+            holder.rowMainBinding.textViewRowMainUserNickname.text = "작성자 $position"
+        }
+    }
+
+    // 검색 화면 RecyclerView Adapter
+    inner class SearchRecyclerViewAdapter() : RecyclerView.Adapter<SearchRecyclerViewAdapter.SearchViewHolder>() {
+        inner class SearchViewHolder(rowMainBinding: RowMainBinding) : RecyclerView.ViewHolder(rowMainBinding.root) {
+            val rowMainBinding: RowMainBinding
+
+            init {
+                this.rowMainBinding = rowMainBinding
+
+                this.rowMainBinding.root.layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT
+                )
+            }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchViewHolder {
+            val rowMainBinding = RowMainBinding.inflate(layoutInflater)
+            val searchViewHolder = SearchViewHolder(rowMainBinding)
+
+            return searchViewHolder
+        }
+
+        override fun getItemCount(): Int {
+            return 10
+        }
+
+        override fun onBindViewHolder(holder: SearchViewHolder, position: Int) {
+            holder.rowMainBinding.textViewRowMainTitle.text = "제목 $position"
+            holder.rowMainBinding.textViewRowMainUserNickname.text = "작성자 $position"
         }
     }
 }
