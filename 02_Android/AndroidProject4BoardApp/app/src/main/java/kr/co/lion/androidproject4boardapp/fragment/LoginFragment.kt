226e7025bbff5cb7178b700858cf615c793252ee
rmadmsjm <dmswjdals12@naver.com>
2024-03-12 09:38:32 +0900

    240312 LoginFragment viewModel, 유효성 검사

--- 02_Android/AndroidProject4BoardApp/app/src/main/java/kr/co/lion/androidproject4boardapp/fragment/LoginFragment.kt
+++ 02_Android/AndroidProject4BoardApp/app/src/main/java/kr/co/lion/androidproject4boardapp/fragment/LoginFragment.kt
@@ -6,25 +6,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import kr.co.lion.androidproject4boardapp.ContentActivity
 import kr.co.lion.androidproject4boardapp.MainActivity
 import kr.co.lion.androidproject4boardapp.MainFragmentName
 import kr.co.lion.androidproject4boardapp.R
+import kr.co.lion.androidproject4boardapp.Tools
 import kr.co.lion.androidproject4boardapp.databinding.FragmentLoginBinding
+import kr.co.lion.androidproject4boardapp.viewmodel.LoginViewModel
 
 class LoginFragment : Fragment() {
 
     lateinit var fragmentLoginBinding: FragmentLoginBinding
     lateinit var mainActivity: MainActivity
+    lateinit var loginViewModel: LoginViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        fragmentLoginBinding = FragmentLoginBinding.inflate(inflater)
+        fragmentLoginBinding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_login, container, false)
+        loginViewModel = LoginViewModel()
+        fragmentLoginBinding.loginViewModel = loginViewModel
+        fragmentLoginBinding.lifecycleOwner = this
         mainActivity = activity as MainActivity
 
         settingToolbar()
         settingButtonLoginJoin()
         settingButtonLoginSubmit()
+        checkInputForm()
 
         return fragmentLoginBinding.root
     }
@@ -56,14 +64,39 @@ class LoginFragment : Fragment() {
         fragmentLoginBinding.apply {
             buttonLoginSubmit.apply {
                 setOnClickListener {
-                    // ContentActivity 실행
-                    val contentIntent = Intent(mainActivity, ContentActivity::class.java)
-                    startActivity(contentIntent)
+                    // 유효성 검사
+                    val chk = checkInputForm()
 
-                    // MainActivity 종료
-                    mainActivity.finish()
+                    // 모든 유효성 검사에 통과를 했다면
+                    if(chk == true) {
+                        // ContentActivity 실행
+                        val contentIntent = Intent(mainActivity, ContentActivity::class.java)
+                        startActivity(contentIntent)
+
+                        // MainActivity 종료
+                        mainActivity.finish()
+                    }
                 }
             }
         }
+    }
+
+    // 유효성 검사
+    fun checkInputForm(): Boolean {
+        // 입력한 값 가져오기
+        val userId = loginViewModel.textFieldLoginUserId.value!!
+        val userPw = loginViewModel.textFieldLoginUserPw.value!!
+
+        if(userId.isEmpty()) {
+            Tools.showErrorDialog(mainActivity, fragmentLoginBinding.textFieldLoginUserId, "아이디 입력 오류", "아이디를 입력해주세요")
+            return false
+        }
+
+        if(userPw.isEmpty()) {
+            Tools.showErrorDialog(mainActivity, fragmentLoginBinding.textFieldLoginUserPw, "비밀번호 입력 오류", "비밀번호를 입력해주세요")
+            return false
+        }
+
+        return true
     }
 }
