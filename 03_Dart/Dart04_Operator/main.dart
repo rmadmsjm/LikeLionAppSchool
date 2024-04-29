679de2d5d3d19c012a5288418a526a06d347597c
rmadmsjm <dmswjdals12@naver.com>
2024-04-29 14:01:46 +0900

    240429 연산자

---
+++ 03_Dart/Dart04_Operator/main.dart
@@ -0,0 +1,46 @@
+void main() {
+  // null을 허용하는 변수
+  // var 변수는 허용되지 않음
+  int? a1 = null;
+  dynamic? a2 = null;
+
+  print('a1 : $a1');
+  print('a2 : $a2');
+
+  // null을 허용하는 변수에 값 저장
+  a1 = 100;
+  a2 = 200;
+
+  print('a1 : $a1');
+  print('a2 : $a2');
+
+  // ?를 사용하지 않은 변수는 null을 담을 수 없음
+  // int a3 = null;
+
+  int? a3 = null;
+  print("a3 : $a3");
+
+  a3 ??= 100;
+  print("a3 : $a3");
+
+  // a3에는 null이 아닌 100이 저장되어 있기 때문에 200이 저장되지 않음
+  a3 ??= 200;
+  print("a3 : $a3");
+
+  // is 연산자
+  // 변수의 자료형을 확인할 수 있음
+  int a4 = 100;
+  var a5 = 100;
+  dynamic a6 = 100;
+
+  print(a4 is int);
+  print(a4 is double);
+  print(a5 is int);
+  print(a5 is double);
+  print(a6 is int);
+  print(a6 is double);
+
+  a6 = 11.11;
+  print(a6 is int);
+  print(a6 is double);
+}
